ee803666ab47b419f8645cc49e05dbfa0d5c0a03
Mạnh Hoàng <48275146+manhhoang21@users.noreply.github.com>
2019-05-22 19:52:41 +0700

    Add files via upload
    Class Constants dùng để khai báo id, secret và redirect uri của ứng dụng
    Class RestFB dùng để truy vấn tới tài khoản facebook và lấy thông tin tài khoản facebook

---
+++ src/model/Constants.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Constants {
+	public static String FACEBOOK_APP_ID = "2205786899508995";
+	  public static String FACEBOOK_APP_SECRET = "4cdf68e4959c4e0b381d7a30259b9f18";
+	  public static String FACEBOOK_REDIRECT_URL = "https://chauvanson.azurewebsites.net/login-facebook";
+	  public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
+}
